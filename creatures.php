361004b463fe9dad065d330058ba9b64fa35c23b
Arth Pajor <adrasi@gmail.com>
2020-09-29 13:37:29 +0100

    trying out grid

--- creatures.php
+++ creatures.php
@@ -2,19 +2,15 @@
 
 $db = new PDO('mysql:host=db; dbname=AnimeCreatures', 'root', 'password');
 
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare('SELECT `name`, `movie`, `year`, `special_abilities`, `creepiness`, `img_dir` FROM `creatures`;');
-$query->execute();
-$collection = $query->fetchAll();
-
-
-
-echo "<table>";
-
-foreach ($collection as $creature) {
-    echo  "<tr><td>" . "Name: " . $creature['name'] . "</td>" . "<td>" . "Movie: " . $creature['movie'] . "</td>"
-        . "<td>" . "Year: " . $creature['year'] . "</td>" . "<td>" . "Special Ability: " . $creature['special_abilities'] . "</td>"
-        . "<td>" . "How scary: " . $creature['creepiness'] . "</td>" . "<td>" . $creature['img_dir'] . "</td></tr>";
+function display($db)
+{
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $query = $db->prepare('SELECT `name`, `movie`, `year`, `special_abilities`, `creepiness` FROM `creatures`;');
+    $query->execute();
+    $collection = $query->fetchAll();
+    foreach ($collection as $creature) {
+        echo "Name: " . $creature['name'] .  "Movie: " . $creature['movie'] . "Year: " . $creature['year']
+            . "Special Ability: " . $creature['special_abilities'] . "How scary: " . $creature['creepiness'];
+    }
 }
 
-echo "</table>";
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Studio Ghibli Creatures</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="creatures.css" />
+</head>
+<body>
+<?php require 'creatures.php'; ?>
+<header><h1>Studio Ghibli Creatures Collection</h1></header>
+
+<section class="monsters">
+    <?php display($db); ?>
+    <div id="totoro"></div>
+    <div id="ohmu"></div>
+    <div id="kodama"></div>
+    <div id="noface"></div>
+    <div id="spirit"></div>
+    <div id="calcifer"></div>
+
+
+
+    <form>
+
+    </form>
+</section>
+
+
+
+
+
+
+</body>
+</html>
