688f3c21eafc6302359cb0bb30a54d148644c8fb
Arth Pajor <adrasi@gmail.com>
2020-09-29 11:08:16 +0100

    created html and css files, starting to work on layout

--- creatures.php
+++ creatures.php
@@ -1,13 +1,20 @@
 <?php
 
-$db = new PDO('mysql:host=db; dbname=GhibliCreatures', 'root', 'password');
+$db = new PDO('mysql:host=db; dbname=AnimeCreatures', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-$query = $db->prepare('SELECT `name`, `movie`, `year`, `superpower`, `creepiness`, `img_dir` FROM `creatures`;');
-
+$query = $db->prepare('SELECT `name`, `movie`, `year`, `special_abilities`, `creepiness`, `img_dir` FROM `creatures`;');
 $query->execute();
-
 $collection = $query->fetchAll();
 
-var_dump($collection);
+
+
+echo "<table>";
+
+foreach ($collection as $creature) {
+    echo  "<tr><td>" . "Name: " . $creature['name'] . "</td>" . "<td>" . "Movie: " . $creature['movie'] . "</td>"
+        . "<td>" . "Year: " . $creature['year'] . "</td>" . "<td>" . "Special Ability: " . $creature['special_abilities'] . "</td>"
+        . "<td>" . "How scary: " . $creature['creepiness'] . "</td>" . "<td>" . $creature['img_dir'] . "</td></tr>";
+}
+
+echo "</table>";
