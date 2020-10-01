f0a6bce67408539beffbc0d9221bda6d3462ba72
Arth Pajor <adrasi@gmail.com>
2020-10-01 16:24:02 +0100

    added forms

--- form.php
+++ form.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <?php
-    $db = new PDO('mysql:host=db; dbname=AnimeCreatures', 'root', 'password');
-    require 'creatures.php'; ?>
+    $db = new PDO('mysql:host=db; dbname=Anime', 'root', 'password'); ?>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="normalize.css" />
     <link rel="stylesheet" type="text/css" href="form.css" />
@@ -11,7 +10,7 @@
 </head>
 <body>
 <h1>You can add your favourite creature to this collection:</h1>
-<form method="post" action="index.php" id="add_form" enctype="multipart/form-data">
+<form method="post" action="creatures.php" id="add_form" enctype="multipart/form-data">
     <div>
         <label for="name">Add your creature's name:</label><br>
         <input id="name" name="name" type="text" value="" size="30" /><br>
@@ -33,8 +32,9 @@
         <input id="creepiness" name="creepiness" type="number" size="30" /><br>
     </div>
     <div>
-        <button type="submit" value="Add">Add</button>
+        <button type="submit" value="add" name="add">Add</button>
     </div>
 </form>
 </body>
 </html>
+
